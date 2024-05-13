b9e7188509358154b3ad6b10e8e241496615f55e
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2024-05-13 18:04:20 +0600

    404

---
+++ resources/views/backend/layouts/Settings/errors/404.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="backend/images/favicon.svg" type="image/x-icon" />
+    <title>Error Page | PlainAdmin Demo</title>
+
+    <link rel="stylesheet" href="backend/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="backend/css/lineicons.css" />
+    <link rel="stylesheet" href="backend/css/quill/bubble.css" />
+    <link rel="stylesheet" href="backend/css/quill/snow.css" />
+    <link rel="stylesheet" href="backend/css/fullcalendar.css" />
+    <link rel="stylesheet" href="backend/css/morris.css" />
+    <link rel="stylesheet" href="backend/css/datatable.css" />
+    <link rel="stylesheet" href="backend/css/main.css" />
+
+    <style>
+        .error-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="preloader">
+        <div class="spinner"></div>
+    </div>
+
+
+
+
+    <div class="error-container">
+        <div class="error-box">
+            <img class="mx-auto mb-20" src="backend/images/error.svg" alt="error img">
+            <h1 class="fw-700 mb-15">Page Not Found</h1>
+            <p class="text-sm mb-25">
+                The page you are looking for was moved removed.
+                rename or might never existed.
+            </p>
+
+            <div class="d-flex align-items-center justify-content-center gap-3">
+                <a href="index.html" class="main-btn primary-btn btn-hover">Go Home</a>
+                <a href="index.html" class="main-btn secondary-btn btn-hover">Contact Us</a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <script src="backend/js/bootstrap.bundle.min.js"></script>
+    <script src="backend/js/Chart.min.js"></script>
+    <script src="backend/js/apexcharts.min.js"></script>
+    <script src="backend/js/dynamic-pie-chart.js"></script>
+    <script src="backend/js/moment.min.js"></script>
+    <script src="backend/js/fullcalendar.js"></script>
+    <script src="backend/js/jvectormap.min.js"></script>
+    <script src="backend/js/world-merc.js"></script>
+    <script src="backend/js/polyfill.js"></script>
+    <script src="backend/js/quill.min.js"></script>
+    <script src="backend/js/datatable.js"></script>
+    <script src="backend/js/Sortable.min.js"></script>
+    <script src="backend/js/main.js"></script>
+</body>
+
+</html>
